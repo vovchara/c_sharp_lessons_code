a6b734d24d28297c62b284a961a6762d714c3091
Volodymyr Martyniuk <vmartyniuk@playtika.com>
2021-11-22 17:17:39 +0200

    forms test 1

---
+++ WinFormsApp1/WinFormsApp1/MainController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    class MainController
+    {
+        private readonly Form _mainForm;
+
+        public MainController(Form mainForm)
+        {
+            _mainForm = mainForm;
+        }
+
+        public void Run()
+        {
+            Button helloButton = new Button();
+            helloButton.BackColor = Color.LightGray;
+            helloButton.ForeColor = Color.DarkGray;
+            helloButton.Location = new Point(10, 10);
+            helloButton.Text = "Привет";
+            _mainForm.Controls.Add(helloButton);
+            helloButton.Click += OnClicked;
+        }
+
+        private void OnClicked(object sender, EventArgs e)
+        {
+            _mainForm.Text = "OOOOOOO";
+            var myForm = new UserControl1();
+            myForm.Name = "AAAAAAAAAAAAA";
+            _mainForm.Controls.Add(myForm);
+        }
+    }
+}
