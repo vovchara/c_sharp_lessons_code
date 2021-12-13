4d7addf4cc661399ebe97ea589bf08ea2583e377
Volodymyr Martyniuk <vmartyniuk@playtika.com>
2021-12-13 16:48:13 +0200

    metanit mvvm example https://metanit.com/sharp/wpf/22.4.php

---
+++ MVVMProj0/MVVMProj0/model/Phone.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMProj0.model
+{
+    public class Phone : INotifyPropertyChanged
+    {
+        private string title;
+        private string company;
+        private int price;
+
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Company
+        {
+            get { return company; }
+            set
+            {
+                company = value;
+                OnPropertyChanged();
+            }
+        }
+        public int Price
+        {
+            get { return price; }
+            set
+            {
+                price = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
